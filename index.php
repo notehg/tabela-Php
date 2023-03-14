444cb141173a6533fadf251f4c0ae0c12caf35bb
notehg <110551666+notehg@users.noreply.github.com>
2023-03-14 12:17:22 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link real="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Teste php</h1>
+    <?php
+    echo "Olá mundo"
+
+?>
+    
+<script src="script.js"></script>
+</body>
+</html>
